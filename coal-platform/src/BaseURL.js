1ef72c1c9db0161511244c0ba52a5c2b57600fbd
Venerablevivek <venerablevivekch@gmail.com>
2024-05-02 17:02:36 +0530

    updated

--- coal-platform/src/BaseURL.js
+++ coal-platform/src/BaseURL.js
@@ -1,2 +1,2 @@
-// export const BASE_URL = "https://crm-r4zo.onrender.com/api/v1"
-export const BASE_URL = "http://localhost:4000/api/v1"
+export const BASE_URL = "https://crm-r4zo.onrender.com/api/v1"
+// export const BASE_URL = "http://localhost:4000/api/v1"
