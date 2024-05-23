afb8c91ecf24c7197bb73a7d608128985b893ddf
shivam malhotra <141710462+Zupinn18@users.noreply.github.com>
2024-05-23 16:49:05 +0530

    u

--- coal-platform/src/BaseURL.js
+++ coal-platform/src/BaseURL.js
@@ -1,3 +1,3 @@
 // export const BASE_URL = "http://65.0.7.147:8000/api/v1"
 // export const BASE_URL = "http://localhost:8000/api/v1"
-export const BASE_URL = "http://13.201.25.209:8000/api/v1"
+export const BASE_URL = "https://crm-ldxw.onrender.com/api/v1"
