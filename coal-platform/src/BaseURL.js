4e290fe5f13d78b5e2d69ce2a1fa540f73b7e89d
shivam malhotra <141710462+Zupinn18@users.noreply.github.com>
2024-05-24 12:50:53 +0530

    U

--- coal-platform/src/BaseURL.js
+++
@@ -1,3 +0,0 @@
-// export const BASE_URL = "http://65.0.7.147:8000/api/v1"
-// export const BASE_URL = "http://localhost:8000/api/v1"
-export const BASE_URL = "https://crm-ldxw.onrender.com/api/v1"
--- coal-platform/src/utils/formateDate.jsx
+++
@@ -1,4 +0,0 @@
-export const formateDate = (date) =>{
-    var myDate = new Date(date);
-    return myDate.toLocaleDateString();
-};
